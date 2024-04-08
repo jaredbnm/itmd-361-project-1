020b1f897c7a7e288aee5b4ee7c0a1d676644159
jaredbnm <jaredjr0204@gmail.com>
2024-04-08 18:35:54 -0500

    adding some style

--- js/map.js
+++ js/map.js
@@ -2,9 +2,10 @@ var map;
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
-    center: {lat: 41.8349, lng: -87.6270},
+    center: {lat: 41.83584308012992, lng: -87.62586149478142},
     zoom: 13,
   });
-
   
+
 }
+google.maps.event.addDomListener(window, 'load', initMap)
