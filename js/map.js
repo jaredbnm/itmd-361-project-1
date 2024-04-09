9afedf5436cf9cbd75695536b9c5ab45a84e2083
jaredbnm <jaredjr0204@gmail.com>
2024-04-08 19:23:24 -0500

    added marker and zoom in

--- js/map.js
+++ js/map.js
@@ -3,9 +3,29 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
     center: {lat: 41.83584308012992, lng: -87.62586149478142},
-    zoom: 13,
+    zoom: 10,
   });
-  
+
+  const marker = new google.maps.Marker({
+    position: {lat: 41.83584308012992, lng: -87.62586149478142},
+    map: map,
+    title: "MTCC"
+  });
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: "This is the McCormick Tribune Campus Center.",
+  });
+  infoWindow.open(map, marker);
+
+  map.addListener("center_changed", () => {
+    window.setTimeout(() => {
+      map.panTo(marker.position);
+    }, 3000);
+  });
+  marker.addListener("click", () => {
+    map.setZoom(17);
+    map.setCenter(marker.position);
+  });
 
 }
 google.maps.event.addDomListener(window, 'load', initMap)
