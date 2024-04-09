9a14bde4136a4ec3665d5416b7ccda21cbc21f5a
jaredbnm <jaredjr0204@gmail.com>
2024-04-09 12:09:13 -0500

    changed mapId

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@ var map;
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
+    mapId: "fc3dca03f9606ead",
     center: {lat: 41.83584308012992, lng: -87.62586149478142},
     zoom: 10,
   });
@@ -21,7 +22,7 @@ async function initMap() {
   map.addListener("center_changed", () => {
     window.setTimeout(() => {
       map.panTo(marker.position);
-    }, 3000);
+    }, 2000);
   });
   marker.addListener("click", () => {
     map.setZoom(17);
